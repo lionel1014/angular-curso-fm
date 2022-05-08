72a8a468f18eae2b615437f4309ad89581a08267
Lionel Diez Gómez <satur.1512@gmail.com>
2022-05-08 16:33:35 -0300

    contenido de la tabla dinamico

--- 03-paisesAppPro/src/app/paises/pages/por-pais/por-pais.component.ts
+++ 03-paisesAppPro/src/app/paises/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/county-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,18 +11,25 @@ export class PorPaisComponent {
 
   termino!:string;
   isHttpOk: boolean = true
+  paises: Array<Country> = [];
 
   constructor( private paisService: PaisService ) { }
 
   buscar(){
     
+    if (this.termino.length < 2) {
+      return
+    }
+
     this.isHttpOk = true
     this.paisService.buscarPais(this.termino).subscribe( (response) =>{
       console.log(response);
+      this.paises = response;
 
     }, (err) =>{
       console.info(err);
       this.isHttpOk = false;
+      this.paises
     });
   }
 
